824b032505cd0e5dea01f48cd363f20095dafc59
“rohitumadi” <rohitumadi2000@gmail.com>
2023-11-06 19:46:31 +0530

    user-service registered on eureka

--- userService/src/main/java/com/hotel/user/service/UserServiceApplication.java
+++ userService/src/main/java/com/hotel/user/service/UserServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient annotation is not required anymore.
+//		Simply adding spring-cloud-starter-netflix-eureka-client
+//		to dependencies will enable the client
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
