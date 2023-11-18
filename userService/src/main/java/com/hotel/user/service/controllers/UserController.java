01ac62f886e6065f7a33a3e63c215cc8c1913376
rohitumadi <rohitumadi2000@gmail.com>
2023-11-18 14:57:31 +0530

    retry implemented

--- userService/src/main/java/com/hotel/user/service/controllers/UserController.java
+++ userService/src/main/java/com/hotel/user/service/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.hotel.user.service.controllers;
 
 import com.hotel.user.service.entities.User;
 import com.hotel.user.service.services.UserService;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+    private Logger logger= LoggerFactory.getLogger(UserController.class);
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -23,12 +27,32 @@ public class UserController {
 
     }
 
-
+    int retryCount=1;
     @GetMapping("/{userId}")
+//    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    @Retry(name="ratingHotelService",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUser(@PathVariable String userId){
+        logger.info("Get Single User Handler: UserController");
+        logger.info("Retry count: {}",retryCount);
+        retryCount++;
         User user=this.userService.getUserById(userId);
         return  new ResponseEntity<User>(user, HttpStatus.OK);
     }
+
+
+    public ResponseEntity<User> ratingHotelFallback( String userId,Exception ex)
+    {
+//        logger.info("Fallback is executed because service is down",ex.getMessage());
+
+        User user=User.builder().userEmail("dummy@gmail.com")
+                .userName("dummy")
+                .about("created as dummy because some service is down")
+                .userId("12345").build();
+
+        return new ResponseEntity<>(user,HttpStatus.OK);
+
+    }
+
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUser(){
